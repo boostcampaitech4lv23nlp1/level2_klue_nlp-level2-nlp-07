6e95f49d24e59d79caa6e44b1e910140c4aeeb56
neulvo <kzvxy@naver.com>
2022-11-28 09:34:56 +0000

    feat: duplicate binary for multi_classifier

---
+++ multi_binary/utils_for_per.py
@@ -0,0 +1,107 @@
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+import sklearn
+import numpy as np
+import pickle as pickle
+from torch import nn
+import torch
+
+from transformers import Trainer
+import math
+from torch.optim.lr_scheduler import _LRScheduler,CosineAnnealingWarmRestarts
+
+
+def klue_re_micro_f1(preds, labels):
+    """KLUE-RE micro f1 (except no_relation)"""
+    label_list = ['per:title', 
+       'per:employee_of', 'per:product', 'per:children',
+       'per:place_of_residence', 'per:alternate_names',
+       'per:other_family', 'per:colleagues', 'per:origin', 'per:siblings',
+       'per:spouse', 'per:parents',
+       'per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
+       'per:place_of_birth', 'per:place_of_death', 'per:religion']
+    
+    # no_relation class를 제외한 micro F1 score
+    # no_relation_label_idx = label_list.index("no_relation")
+    label_indices = list(range(len(label_list)))
+    # label_indices.remove(no_relation_label_idx)
+                         
+    return sklearn.metrics.f1_score(labels, preds, average="micro", labels=label_indices) * 100.0
+
+def klue_re_auprc(probs, labels):
+    """KLUE-RE AUPRC (with no_relation)"""
+    labels = np.eye(18)[labels]
+
+    score = np.zeros((18,))
+    for c in range(18):
+        targets_c = labels.take([c], axis=1).ravel()
+        preds_c = probs.take([c], axis=1).ravel()
+        precision, recall, _ = sklearn.metrics.precision_recall_curve(targets_c, preds_c)
+        score[c] = sklearn.metrics.auc(recall, precision)
+    return np.average(score) * 100.0
+
+def compute_metrics(pred):
+    """ validation을 위한 metrics function """
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    probs = pred.predictions
+
+    # calculate accuracy using sklearn's function
+    f1 = klue_re_micro_f1(preds, labels)
+    auprc = klue_re_auprc(probs, labels)
+    acc = accuracy_score(labels, preds) # 리더보드 평가에는 포함되지 않습니다.
+
+    return {
+      'micro_f1_score': f1,
+      'auprc' : auprc,
+      'accuracy': acc,
+    }
+
+def label_to_num(label):
+    num_label = []
+    # dict_label_to_num = {'no_relation' : 0, 'relation' : 1}
+    # with open('/opt/ml/code/dict_label_to_num.pkl', 'rb') as f:
+    #     dict_label_to_num = pickle.load(f)
+    per_label_list = ['per:title', 
+       'per:employee_of', 'per:product', 'per:children',
+       'per:place_of_residence', 'per:alternate_names',
+       'per:other_family', 'per:colleagues', 'per:origin', 'per:siblings',
+       'per:spouse', 'per:parents','per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
+       'per:place_of_birth', 'per:place_of_death', 'per:religion']
+    dict_label_to_num = {v :i for i,v in enumerate(per_label_list)}
+
+    for v in label:
+        num_label.append(dict_label_to_num[v])
+
+    return num_label
+
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        ce_loss = nn.CrossEntropyLoss()(inputs, targets)
+        pt = torch.exp(-ce_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * ce_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
+
+class TrainerWithLossTuning(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("labels")
+        # forward pass
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+        # compute custom loss (suppose one has 3 labels with different weights)
+        # loss_fct = nn.CrossEntropyLoss()
+        loss_fct = FocalLoss()
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        return (loss, outputs) if return_outputs else loss
