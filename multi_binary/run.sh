c703056e8acb559bea8e5e2b71d97d4e3448d318
neulvo <kzvxy@naver.com>
2022-11-28 10:43:27 +0000

    feat: utils ongoing

--- multi_binary/run.sh
+++
@@ -1,3 +0,0 @@
-python3 binary_train.py
-python3 train_for_org.py
-python3 train_for_per.py
--- multi_binary/utils.py
+++ multi_binary/utils.py
@@ -7,7 +7,117 @@ import torch
 
 from transformers import Trainer
 
-def klue_re_micro_f1(preds, labels):
+ID_TO_TYPE_PAIR = {
+    0: 'ORG_PER',1: 'ORG_ORG',2: 'ORG_DAT',3: 'ORG_LOC',4: 'ORG_POH',5: 'ORG_NOH',
+    6: 'PER_PER',7: 'PER_ORG',8: 'PER_DAT',9: 'PER_LOC',10: 'PER_POH',11: 'PER_NOH'
+}
+
+LABEL_TO_ID = {
+    0: # ORG_PER
+    {
+        'org:top_members/employees': 0,
+        'org:founded_by': 1,
+        'org:alternate_names': 2,
+    },
+    1: # ORG_ORG
+    {
+        'org:member_of': 0,
+        'org:alternate_names': 1,
+        'org:members': 2,
+        'org:place_of_headquarters': 3,
+        'org:political/religious_affiliation': 4,
+        'org:product': 5,
+        'org:top_members/employees' : 6,
+        'org:founded_by' : 7,
+    },
+    2: # ORG_DAT
+    {
+        'org:founded': 0,
+        'org:dissolved': 1,
+    },
+    3: # ORG_LOC
+    {
+        'org:place_of_headquarters': 0,
+        'org:country_of_headquarters': 1,
+        'org:member_of': 2,
+        'org:members': 3,
+        'org:product': 4,
+        'org:alternate_names': 5,
+    },
+    4: # ORG_POH
+    {
+        'org:member_of': 0,
+        'org:product': 1,
+        'org:alternate_names': 2,
+        'org:top_members/employees': 3,
+        'org:place_of_headquarters': 4,
+        'org:political/religious_affiliation': 5,
+        'org:members':6,
+        'org:founded_by':7,
+    },
+    5: # ORG_NOH
+    {
+        'org:number_of_employees/members': 0,
+        'org:member_of' :1,
+    },
+    6: # PER_PER
+    {
+        'per:alternate_names': 0,
+        'per:spouse': 1,
+        'per:colleagues': 2,
+        'per:parents': 3,
+        'per:employee_of': 4,
+        'per:children': 5,
+        'per:other_family':6,
+        'per:siblings':7,
+        'per:origin': 8,  
+        'per:title':9,
+    },
+    7: # PER_ORG
+    {
+        'per:employee_of': 0,
+        'per:origin': 1,
+        'per:title':2,
+        'per:schools_attended':3,
+        'per:religion': 4,
+        'per:alternate_names': 5, 
+    },
+    8: # PER_DAT
+    {
+        'per:date_of_birth': 0,
+        'per:date_of_death': 1,
+        'per:origin': 2,
+        'per:employee_of': 3,
+    },
+    9: # PER_LOC
+    {
+        'per:origin': 0,
+        'per:place_of_residence': 1,
+        'per:employee_of': 2,
+        'per:place_of_birth': 3,
+        'per:title': 4,
+        'per:place_of_death': 5,
+        'per:alternate_names': 6,
+    },
+    10: # PER_POH
+    {
+        'per:title': 0,
+        'per:employee_of': 1,
+        'per:product': 2,
+        'per:alternate_names':3,
+        'per:parents':4,
+        'per:origin':5,
+        'per:spouse':6,
+        'per:siblings':7,
+        'per:children':8,
+    },
+    11: # PER_NOH
+    {
+        'per:title': 0,
+        'per:employee_of': 1,
+    },}
+
+def bi_klue_re_micro_f1(preds, labels):
     """KLUE-RE micro f1 (except no_relation)"""
     label_list = ['no_relation', 'org:top_members/employees', 'org:members',
        'org:product', 'per:title', 'org:alternate_names',
@@ -20,14 +130,14 @@ def klue_re_micro_f1(preds, labels):
        'per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
        'per:place_of_birth', 'per:place_of_death', 'org:founded_by',
        'per:religion']
-    
+
     # no_relation class를 제외한 micro F1 score
     no_relation_label_idx = label_list.index("no_relation")
     label_indices = list(range(len(label_list)))
     label_indices.remove(no_relation_label_idx)
     return sklearn.metrics.f1_score(labels, preds, average="micro", labels=label_indices) * 100.0
 
-def klue_re_auprc(probs, labels):
+def bi_klue_re_auprc(probs, labels):
     """KLUE-RE AUPRC (with no_relation)"""
     labels = np.eye(30)[labels]
 
@@ -39,15 +149,38 @@ def klue_re_auprc(probs, labels):
         score[c] = sklearn.metrics.auc(recall, precision)
     return np.average(score) * 100.0
 
-def compute_metrics(pred):
+def multi_klue_re_micro_f1(preds, labels, type_pair_id):
+    """KLUE-RE micro f1 (except no_relation)"""
+    label_list = list(LABEL_TO_ID(type_pair_id).keys())
+
+    # no_relation class를 제외한 micro F1 score
+    # no_relation_label_idx = label_list.index("no_relation")
+    label_indices = list(range(len(label_list)))
+    # label_indices.remove(no_relation_label_idx)
+    return sklearn.metrics.f1_score(labels, preds, average="micro", labels=label_indices) * 100.0
+
+def multi_klue_re_auprc(probs, labels, type_pair_id):
+    """KLUE-RE AUPRC (with no_relation)"""
+    num_labels = len(LABEL_TO_ID[type_pair_id])
+    labels = np.eye(num_labels)[labels]
+
+    score = np.zeros((num_labels,))
+    for c in range(num_labels):
+        targets_c = labels.take([c], axis=1).ravel()
+        preds_c = probs.take([c], axis=1).ravel()
+        precision, recall, _ = sklearn.metrics.precision_recall_curve(targets_c, preds_c)
+        score[c] = sklearn.metrics.auc(recall, precision)
+    return np.average(score) * 100.0
+
+def compute_metrics(pred, ):
     """ validation을 위한 metrics function """
     labels = pred.label_ids
     preds = pred.predictions.argmax(-1)
     probs = pred.predictions
 
     # calculate accuracy using sklearn's function
-    f1 = klue_re_micro_f1(preds, labels)
-    auprc = klue_re_auprc(probs, labels)
+    f1 = multi_klue_re_micro_f1(preds, labels)
+    auprc = multi_klue_re_auprc(probs, labels)
     acc = accuracy_score(labels, preds) # 리더보드 평가에는 포함되지 않습니다.
 
     return {
